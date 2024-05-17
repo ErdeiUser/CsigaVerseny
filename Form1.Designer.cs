042a617707e0243eaf1d0a57f6ca371f38d4104d
Erdei Lajos <erdei.lajos@infoscope.hu>
2024-05-17 15:21:20 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace CsigaVerseny
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.rbConsole = new System.Windows.Forms.RadioButton();
+            this.rbText = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(44, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // rbConsole
+            // 
+            this.rbConsole.AutoSize = true;
+            this.rbConsole.Checked = true;
+            this.rbConsole.Location = new System.Drawing.Point(214, 40);
+            this.rbConsole.Name = "rbConsole";
+            this.rbConsole.Size = new System.Drawing.Size(57, 17);
+            this.rbConsole.TabIndex = 1;
+            this.rbConsole.TabStop = true;
+            this.rbConsole.Tag = "rb";
+            this.rbConsole.Text = "Konzol";
+            this.rbConsole.UseVisualStyleBackColor = true;
+            // 
+            // rbText
+            // 
+            this.rbText.AutoSize = true;
+            this.rbText.Location = new System.Drawing.Point(306, 41);
+            this.rbText.Name = "rbText";
+            this.rbText.Size = new System.Drawing.Size(46, 17);
+            this.rbText.TabIndex = 2;
+            this.rbText.TabStop = true;
+            this.rbText.Tag = "rb";
+            this.rbText.Text = "Text";
+            this.rbText.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 78);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(731, 360);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rbText);
+            this.Controls.Add(this.rbConsole);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RadioButton rbConsole;
+        private System.Windows.Forms.RadioButton rbText;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using CsigaVerseny.Controller;
+using CsigaVerseny.Interface;
+using CsigaVerseny.Modell;
+using CsigaVerseny.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsigaVerseny
+{
+    public partial class Form1 : Form
+    {
+        private Game game;
+        
+        private ICsigaView View;
+        private RaceController controller;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(rbConsole.Checked)
+            {
+                View = new ConsoleView();
+            }
+            else
+            {
+                View = new TextView();
+            }
+            controller = new RaceController(View, game);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            game = new Game();
+            game.StartGame();
+        }
+    }
+}
